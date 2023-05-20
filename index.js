042a73fc1b306b78d724869d6e1c531ff3331cdc
getahsaan <getahsaan@gamil.com>
2023-05-20 20:02:11 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+// middleware
+app.use(cors());
+app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ky76see.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
 
@@ -22,10 +26,9 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    app.get("/toy", (req, res)=>{
-        res.send("Vercel connected Successful")
-    })
 
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
